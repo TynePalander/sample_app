55a7c6924b6c9f80d8d610288187f1c7bc9545cf
Tyne Palander <tyne.palander@fgh.me>
2012-07-30 16:50:19 -0500

    exercise 6

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_filter :signed_in_user, only: [:edit, :index, :update, :destroy]
   before_filter :correct_user,   only: [:edit, :update]
   before_filter :admin_user,     only: :destroy
+  before_filter :unsigned_user,  only: [:new, :create]
 
   def show
     @user = User.find(params[:id])
@@ -62,4 +63,8 @@ class UsersController < ApplicationController
     def admin_user
       redirect_to(root_path) unless current_user.admin?
     end
-end
+
+    def unsigned_user
+      redirect_to(root_path) if signed_in?
+    end
+  end
