b6a6b51d13c0989eef656092ff8463ddc1e0f736
Tyne Palander <tyne.palander@fgh.me>
2012-10-12 16:02:21 -0500

    fix default order by

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,5 @@
 class Tweet < ActiveRecord::Base
   attr_accessible :id_str, :text, :tuser_id_str, :tuser_screen_name, :tweet_created_at
+
+  default_scope order: 'tweets.tweet_created_at DESC'
 end
