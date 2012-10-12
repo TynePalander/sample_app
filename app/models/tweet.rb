39289c77caecfe4bb9ba5f9a625051eadaea38e4
Tyne Palander <tyne.palander@fgh.me>
2012-10-12 16:24:13 -0500

    need to specify DESC for both columns

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,5 @@
 class Tweet < ActiveRecord::Base
   attr_accessible :id_str, :text, :tuser_id_str, :tuser_screen_name, :tweet_created_at
 
-  default_scope order: 'tweets.tweet_created_at, tweets.id_str DESC' # mainly works unless string length changes during that second 
+  default_scope order: 'tweets.tweet_created_at DESC, tweets.id_str DESC' # mainly works unless string length changes during that second 
 end
