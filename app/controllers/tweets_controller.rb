00aaf186b990faceec887cc3f8030126bb8028a6
Tyne Palander <tyne.palander@fgh.me>
2012-10-12 16:38:46 -0500

    limit index of tweets to last 30

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,7 +2,7 @@ class TweetsController < ApplicationController
   # GET /tweets
   # GET /tweets.json
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.find(:all,:limit => 30)
 
     respond_to do |format|
       format.html # index.html.erb
